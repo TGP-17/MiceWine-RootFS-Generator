f77090f2b562305a4e1aec70f5386f8db25acc4d
TGP-17 <mytablet19048@gmail.com>
2025-08-12 11:18:11 +0200

    Update build.sh

--- packages/mesa-zink/build.sh
+++ packages/mesa-zink/build.sh
@@ -4,4 +4,4 @@ PKG_PRETTY_NAME="Mesa Zink Driver"
 
 SRC_URL=https://archive.mesa3d.org/mesa-$PKG_VER.tar.xz
 LDFLAGS="-L$PREFIX/lib -landroid-shmem -lxcb-xfixes"
-MESON_ARGS="-Dgallium-drivers=zink -Dvulkan-drivers= -Dglvnd=enabled -Dplatforms=x11 -Dxmlconfig=enabled -Dllvm=disabled -Dopengl=true -Degl=disabled -Dzstd=enabled" 
+MESON_ARGS="-Dgallium-drivers=freedreno -Dvulkan-drivers= -Dfreedreno-kmds=msm,kgsl -Dglvnd=enabled -Dplatforms=x11 -Dxmlconfig=enabled -Dllvm=disabled -Dopengl=true -Degl=disabled -Dzstd=enabled" 
